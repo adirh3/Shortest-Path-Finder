2f16c786fbd5f01ddd3e39f7f1411442d6756f92
Adir <adirh3@gmail.com>
2019-11-02 16:20:08 +0200

    Changed BFSAlgorithm to be more readable and faster
    Improved CalculatePathAsync of BFSAlgorithm

--- src/ShortestPathFinder.Algorithms.BFS/BfsAlgorithm.cs
+++ src/ShortestPathFinder.Algorithms.BFS/BfsAlgorithm.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using ShortestPathFinder.Common.Algorithm;
@@ -14,6 +13,7 @@ namespace ShortestPathFinder.Algorithms.BFS
     {
         private readonly ILogger<BfsAlgorithm<T>> _logger;
         private readonly IRelationsFinder<T> _relationsFinder;
+        private bool _runAsync = true;
 
         public BfsAlgorithm(ILogger<BfsAlgorithm<T>> logger, IRelationsFinder<T> relationsFinder)
         {
@@ -29,6 +29,18 @@ namespace ShortestPathFinder.Algorithms.BFS
         /// <returns>Enumeration of the <b>shortest</b> path between the source and destination</returns>
         public IEnumerable<Node> CalculatePath(Node source, Node destination)
         {
+            _runAsync = false;
+            return CalculatePathAsync(source, destination).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Asynchronously calculates the <b>shortest</b> path between the specified source and destination nodes 
+        /// </summary>
+        /// <param name="source">The specified source node</param>
+        /// <param name="destination">The specified destination node</param>
+        /// <returns>Enumeration of the <b>shortest</b> path between the source and destination</returns>
+        public async Task<IEnumerable<Node>> CalculatePathAsync(Node source, Node destination)
+        {
             _logger.LogInformation(
                 $"Started BFS algorithm to find the shortest path between {source} and {destination}");
             var sourceNode = source as T;
@@ -36,12 +48,20 @@ namespace ShortestPathFinder.Algorithms.BFS
             var queue = new Queue<T>();
             var previous = new Dictionary<T, T>();
             queue.Enqueue(sourceNode);
-            
+            var result = new List<Node>();
             // Update dictionary for all nodes
             while (queue.Count > 0)
             {
                 var vertex = queue.Dequeue();
-                var relations = _relationsFinder.FindRelations(vertex);
+                if (vertex.Equals(destNode)) // Found destination
+                {
+                    GetPathForResult(result, vertex, previous, sourceNode);
+                    break;
+                }
+
+                var relations = _runAsync
+                    ? await _relationsFinder.FindRelationsAsync(vertex)
+                    : _relationsFinder.FindRelations(vertex);
                 foreach (var node in relations)
                 {
                     if (previous.ContainsKey(node))
@@ -51,36 +71,22 @@ namespace ShortestPathFinder.Algorithms.BFS
                 }
             }
 
-            // Finds the shortest path to the destination
-            Func<T, IEnumerable<T>> shortestPath = v =>
-            {
-                var path = new List<T>();
-
-                var current = v;
-                while (!current.Equals(sourceNode))
-                {
-                    path.Add(current);
-                    current = previous[current];
-                }
-
-                path.Add(v);
-                path.Reverse();
-
-                return path;
-            };
-
-            return shortestPath.Invoke(destNode);
+            return result;
         }
 
-        /// <summary>
-        /// Asynchronously calculates the <b>shortest</b> path between the specified source and destination nodes 
-        /// </summary>
-        /// <param name="source">The specified source node</param>
-        /// <param name="destination">The specified destination node</param>
-        /// <returns>Enumeration of the <b>shortest</b> path between the source and destination</returns>
-        public Task<IEnumerable<Node>> CalculatePathAsync(Node source, Node destination)
+        private static void GetPathForResult(List<Node> result, T vertex, Dictionary<T, T> previous, T source)
         {
-            return Task.Run(() => CalculatePath(source, destination));
+            // Create it's path
+            result.Add(vertex);
+            var curr = vertex;
+            while (!curr.Equals(source))
+            {
+                if (!previous.TryGetValue(curr, out curr))
+                    break;
+                result.Add(curr);
+            }
+
+            result.Reverse(); // Reverse it to get the result from the start
         }
     }
 }
