ee99e71ddbd00c7900d93acc4d7b4adc3128163d
Adir <adirh3@gmail.com>
2019-11-02 10:56:17 +0200

    Added algorithm empty implementation
    Added BFS empty algorithm implementation
    Added PAC empty algorithm implementation
    Changed IRelationFinder and IPathFinderAlgorithm to be generic

---
+++ src/ShortestPathFinder.Algorithm.ParallelCrawl/ParallelCrawlingAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ShortestPathFinder.Common.Algorithm;
+using ShortestPathFinder.Common.Graph;
+
+namespace ShortestPathFinder.Algorithm.ParallelCrawl
+{
+    /// <summary>
+    /// Finds the <b>shortest</b> path between the source and destination by crawling in parallel
+    /// </summary>
+    /// <typeparam name="T">The node type</typeparam>
+    public class ParallelCrawlingAlgorithm<T> : IPathFinderAlgorithm<T> where T : Node
+    {
+        /// <summary>
+        /// Finds the <b>shortest</b> path between the source and destination by crawling in parallel
+        /// </summary>
+        /// <param name="source">The source node</param>
+        /// <param name="destination">The destination node</param>
+        /// <returns>An enumeration of the <b>shortest</b> path between the source and the destination</returns>
+        public IEnumerable<T> CalculatePath(T source, T destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Asynchronously finds the <b>shortest</b> path between the source and destination by crawling in parallel
+        /// </summary>
+        /// <param name="source">The source node</param>
+        /// <param name="destination">The destination node</param>
+        /// <returns>An enumeration of the <b>shortest</b> path between the source and the destination</returns>
+        public Task<IEnumerable<T>> CalculatePathAsync(T source, T destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
