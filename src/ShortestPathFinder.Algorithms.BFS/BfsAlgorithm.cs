f2838fabc053036e3cd0fa5d173914c835c6daff
Adir <adirh3@gmail.com>
2019-11-02 14:37:32 +0200

    Basic implementation of BFS algorithm

--- src/ShortestPathFinder.Algorithms.BFS/BfsAlgorithm.cs
+++ src/ShortestPathFinder.Algorithms.BFS/BfsAlgorithm.cs
@@ -10,7 +10,7 @@ namespace ShortestPathFinder.Algorithms.BFS
     /// <summary>
     /// Breadth first search (BFS) algorithm is used to find the shortest path between 2 nodes in a graph
     /// </summary>
-    public class BfsAlgorithm<T> : IPathFinderAlgorithm where T: Node
+    public class BfsAlgorithm<T> : IPathFinderAlgorithm where T : Node
     {
         private readonly ILogger<BfsAlgorithm<T>> _logger;
         private readonly IRelationsFinder<T> _relationsFinder;
@@ -29,7 +29,47 @@ namespace ShortestPathFinder.Algorithms.BFS
         /// <returns>Enumeration of the <b>shortest</b> path between the source and destination</returns>
         public IEnumerable<Node> CalculatePath(Node source, Node destination)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation(
+                $"Started BFS algorithm to find the shortest path between {source} and {destination}");
+            var sourceNode = source as T;
+            var destNode = destination as T;
+            var queue = new Queue<T>();
+            var previous = new Dictionary<T, T>();
+            queue.Enqueue(sourceNode);
+            
+            // Update dictionary for all nodes
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                var relations = _relationsFinder.FindRelations(vertex);
+                foreach (var node in relations)
+                {
+                    if (previous.ContainsKey(node))
+                        continue;
+                    previous[node] = vertex;
+                    queue.Enqueue(node);
+                }
+            }
+
+            // Finds the shortest path to the destination
+            Func<T, IEnumerable<T>> shortestPath = v =>
+            {
+                var path = new List<T>();
+
+                var current = v;
+                while (!current.Equals(sourceNode))
+                {
+                    path.Add(current);
+                    current = previous[current];
+                }
+
+                path.Add(v);
+                path.Reverse();
+
+                return path;
+            };
+
+            return shortestPath.Invoke(destNode);
         }
 
         /// <summary>
@@ -40,7 +80,7 @@ namespace ShortestPathFinder.Algorithms.BFS
         /// <returns>Enumeration of the <b>shortest</b> path between the source and destination</returns>
         public Task<IEnumerable<Node>> CalculatePathAsync(Node source, Node destination)
         {
-            throw new NotImplementedException();
+            return Task.Run(() => CalculatePath(source, destination));
         }
     }
 }
